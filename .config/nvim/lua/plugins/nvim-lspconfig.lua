578b70e3429967aeda0a27c0fa10197c7ef546d3
David Poling <soraspar78@gmail.com>
2024-09-06 16:39:32 -0400

    Fixed tsserver to ts_ls deprecation warning.

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -167,6 +167,7 @@ return {
 				},
 				handlers = {
 					function(server_name)
+						server_name = server_name == "tsserver" and "ts_ls" or server_name
 						local server = servers[server_name] or {}
 						-- This handles overriding only values explicitly passed
 						-- by the server configuration above. Useful when disabling
